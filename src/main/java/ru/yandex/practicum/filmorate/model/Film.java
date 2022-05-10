d5c9ad4148db317f5d2ea58f8eb5a76efc236a34
golyshev a <golyshev491@gmail.com>
2022-05-10 22:12:47 +0300

    feat: add models, add controllers

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+public class Film {
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
+}
